ef75fc26e79074f586b5f6763504b782698b8ce9
Thomas Fischer <92039475+thomas-c-p-fischer@users.noreply.github.com>
2022-07-29 11:28:33 +0200

    porjet pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { GenerationsComponent } from './components/generations/generations.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { APokemonComponent } from './components/a-pokemon/a-pokemon.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'generations', component: GenerationsComponent},
+  {path: 'pokemons', component: PokemonsComponent}
+  ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenerationsComponent,
+    PokemonsComponent,
+    HomepageComponent,
+    APokemonComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
